389312d7878fcb47595a6299fad990f8433c0306
DM880 <denys.mauro@gmail.com>
2021-10-10 10:41:11 +0000

    projects updated

--- projects/models.py
+++ projects/models.py
@@ -6,7 +6,7 @@ class ProjectDetail(models.Model):
 
     name = models.CharField(max_length=50)
     image = models.ImageField(blank=True, upload_to='projects_img/')
-    description = models.TextField(max_length=100)
+    description = models.TextField(max_length=500)
     github_url = models.URLField(max_length=200)
     live_url = models.URLField(max_length=200, blank=True)
 
