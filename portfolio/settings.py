cc8c7e5f4723c3b5c413aa53b2abf29f32130d97
DM880 <denys.mauro@gmail.com>
2021-10-11 05:47:31 +0000

    settings update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -14,22 +14,18 @@ from pathlib import Path
 import os
 from decouple import config
 from pathlib import Path
-import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config("SECRET_KEY")
 
-SECRET_KEY = os.environ['SECRET_KEY']
+DISABLE_COLLECTSTATIC=1
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -149,4 +145,4 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_HOST = os.environ['EMAIL_HOST']
+EMAIL_HOST = config("EMAIL_HOST")
