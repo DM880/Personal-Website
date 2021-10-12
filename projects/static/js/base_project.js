65011142c30a29540e5147298bf3d8fa4475ae93
DM880 <denys.mauro@gmail.com>
2021-10-12 11:36:46 +0000

    project page responsive

--- projects/static/js/base_project.js
+++ projects/static/js/base_project.js
@@ -3,3 +3,25 @@ $(document).on('click', '#links', function(e){
     var url = $(this).attr('href');
     window.open(url, '_blank');
 });
+
+ $(window).width(function(d) {
+  if ($(window).width() < 950) {
+     $(".odd").addClass("proj-d").removeClass("proj-d-odd");
+     $(".odd-img").addClass("img-cont").removeClass("img-cont-odd");
+  }
+  else{
+     $(".odd").removeClass("proj-d").addClass("proj-d-odd");
+     $(".odd-img").removeClass("img-cont").addClass("img-cont-odd");
+  }
+});
+
+ $(window).resize(function(f) {
+  if ($(window).width() < 950) {
+     $(".odd").addClass("proj-d").removeClass("proj-d-odd");
+     $(".odd-img").addClass("img-cont").removeClass("img-cont-odd");
+  }
+  else{
+     $(".odd").removeClass("proj-d").addClass("proj-d-odd");
+     $(".odd-img").removeClass("img-cont").addClass("img-cont-odd");
+  }
+});
